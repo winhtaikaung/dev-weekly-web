c6a52e7ce78046350e64f5a06a010d6aea0acfe6
winhtaikaung <winhtaikaung28@hotmail.com>
2017-12-15 18:27:50 +0800

    reward detail screen added

---
+++ src/components/rewarddetail/RewardDetailModal.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Modal from 'react-modal';
+
+class RewardDetailModal extends Component{
+
+    constructor(props){
+        super(props)
+    }
+
+    render(){
+        return (<div>
+            I am reward detail
+            </div>)
+    }
+
+}
+
+export default RewardDetailModal;
